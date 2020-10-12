e9c3729c33cb466725dafffb391bbb76d4f359b0
shiryaev.v <shiryaev.v@rozetka.com.ua>
2020-10-12 08:15:47 +0300

    monday:)

---
+++ book/chapterfour/homefour.php
@@ -0,0 +1,15 @@
+<?php
+$vegetables=['corn'=>'yellow',
+             'beet'=>'red',
+             'carrot'=>'orange'];
+
+
+$dinner=[0 => 'Sweet corn',
+         1 => 'LemonChicken',
+         2 => 'Braised Bamboo Fungus'];
+
+print "Vegetables  $vegetables";
+
+        
+
+?>
