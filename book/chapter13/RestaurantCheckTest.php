a12875135bf001165f9511f4d43f6efd1ddc9dbb
shiryaev.v <shiryaev.v@rozetka.com.ua>
2020-11-23 08:45:30 +0200

    zadanie

--- book/chapter13/RestaurantCheckTest.php
+++ book/chapter13/RestaurantCheckTest.php
@@ -1,5 +1,7 @@
 <?php
 include 'restaurant-check.php';
+//require_once ('PHPUnit/Framework/TestCase.php');
+require_once 'phpunit-9.4.3/Autoload.php';
 class RestaurantCheckTest extends PHPUnit_Framework_TestCase {
 	public function testWithTaxAndTip() {
 	$meal = 100;
